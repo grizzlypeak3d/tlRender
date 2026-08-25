a44e043b20cf7f0af1417009992b51069a37b649
Darby Johnston <darbyjohnston@yahoo.com>
2026-08-25 14:55:37 -0700

    Register the icons from the generated map
    Adding an icon took three steps in three files: the CMake list, a
    hand written extern, and an iconSystem->add() line. It is now one:
    drop the file in etc/Icons. The icons go through ftk's
    ftk_icon_resources(), and init() registers whatever the generated
    map holds.

--- lib/tlRender/UI/Init.cpp
+++ lib/tlRender/UI/Init.cpp
@@ -13,37 +13,10 @@
 #include <ftk/UI/Init.h>
 #include <ftk/UI/FileBrowser.h>
 #include <ftk/UI/IconSystem.h>
+
+#include <tl_resource/IconResources.h>
 #include <ftk/GL/System.h>
 
-namespace tl_resource
-{
-    extern std::vector<uint8_t> ColorControls;
-    extern std::vector<uint8_t> ColorPicker;
-    extern std::vector<uint8_t> CompareA;
-    extern std::vector<uint8_t> CompareB;
-    extern std::vector<uint8_t> CompareButterfly;
-    extern std::vector<uint8_t> CompareDifference;
-    extern std::vector<uint8_t> CompareHorizontal;
-    extern std::vector<uint8_t> CompareOverlay;
-    extern std::vector<uint8_t> CompareTile;
-    extern std::vector<uint8_t> CompareVertical;
-    extern std::vector<uint8_t> CompareWipe;
-    extern std::vector<uint8_t> Devices;
-    extern std::vector<uint8_t> Export;
-    extern std::vector<uint8_t> FileOpenAudio;
-    extern std::vector<uint8_t> Files;
-    extern std::vector<uint8_t> Hidden;
-    extern std::vector<uint8_t> Info;
-    extern std::vector<uint8_t> Magnify;
-    extern std::vector<uint8_t> Messages;
-    extern std::vector<uint8_t> PlaybackLoop;
-    extern std::vector<uint8_t> PlaybackOnce;
-    extern std::vector<uint8_t> PlaybackPingPong;
-    extern std::vector<uint8_t> View;
-    extern std::vector<uint8_t> Visible;
-    extern std::vector<uint8_t> WindowSecondary;
-    extern std::vector<uint8_t> tlRender;
-}
 
 namespace tl
 {
@@ -62,32 +35,12 @@ namespace tl
                 FileBrowserThumbnails::create(context));
 
             auto iconSystem = context->getSystem<ftk::IconSystem>();
-            iconSystem->add("ColorControls", tl_resource::ColorControls);
-            iconSystem->add("ColorPicker", tl_resource::ColorPicker);
-            iconSystem->add("CompareA", tl_resource::CompareA);
-            iconSystem->add("CompareB", tl_resource::CompareB);
-            iconSystem->add("CompareButterfly", tl_resource::CompareButterfly);
-            iconSystem->add("CompareDifference", tl_resource::CompareDifference);
-            iconSystem->add("CompareHorizontal", tl_resource::CompareHorizontal);
-            iconSystem->add("CompareOverlay", tl_resource::CompareOverlay);
-            iconSystem->add("CompareTile", tl_resource::CompareTile);
-            iconSystem->add("CompareVertical", tl_resource::CompareVertical);
-            iconSystem->add("CompareWipe", tl_resource::CompareWipe);
-            iconSystem->add("Devices", tl_resource::Devices);
-            iconSystem->add("Export", tl_resource::Export);
-            iconSystem->add("FileOpenAudio", tl_resource::FileOpenAudio);
-            iconSystem->add("Files", tl_resource::Files);
-            iconSystem->add("Hidden", tl_resource::Hidden);
-            iconSystem->add("Info", tl_resource::Info);
-            iconSystem->add("Magnify", tl_resource::Magnify);
-            iconSystem->add("Messages", tl_resource::Messages);
-            iconSystem->add("PlaybackLoop", tl_resource::PlaybackLoop);
-            iconSystem->add("PlaybackOnce", tl_resource::PlaybackOnce);
-            iconSystem->add("PlaybackPingPong", tl_resource::PlaybackPingPong);
-            iconSystem->add("View", tl_resource::View);
-            iconSystem->add("Visible", tl_resource::Visible);
-            iconSystem->add("WindowSecondary", tl_resource::WindowSecondary);
-            iconSystem->add("tlRender", tl_resource::tlRender);
+            // Every icon compiled into the library; the map is generated
+            // from the files in etc/Icons.
+            for (const auto& i : tl_resource::getIconResources())
+            {
+                iconSystem->add(i.first, *i.second);
+            }
         }
     }
 }
