97903afff360905e4d4f2112d4a3000ce24b253a
Darby Johnston <darbyjohnston@yahoo.com>
2026-09-03 19:18:30 -0700

    The special build proves SDL2 now that SDL3 is the default

--- etc/Config/ci-linux-sdl3.cmake
+++
@@ -1,7 +0,0 @@
-# Continuous integration on Linux against SDL3 rather than SDL2.
-include("${CMAKE_CURRENT_LIST_DIR}/local.cmake" OPTIONAL)
-
-set(ftk_SDL2 OFF CACHE BOOL "Build SDL2")
-set(ftk_SDL3 ON CACHE BOOL "Build SDL3")
-
-include("${CMAKE_CURRENT_LIST_DIR}/ci-linux.cmake")
