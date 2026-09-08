7d7e118f8760ac5c382814442948935932fd9831
Darby Johnston <darbyjohnston@yahoo.com>
2026-09-08 15:57:41 -0700

    The libjpeg-turbo tools are left out of the install
    cjpeg, djpeg, jpegtran, rdjpgcom, wrjpgcom and tjbench were installed
    beside the applications. WITH_TOOLS=OFF was being passed, but
    libjpeg-turbo has no such option and ignored it; there is none to
    pass, so the tools are removed after the install instead.

--- etc/SuperBuild/cmake/Modules/Buildlibjpeg-turbo.cmake
+++ etc/SuperBuild/cmake/Modules/Buildlibjpeg-turbo.cmake
@@ -19,7 +19,6 @@ set(libjpeg-turbo_ARGS
     ${TLRENDER_EXTERNAL_ARGS}
     -DENABLE_SHARED=${libjpeg-turbo_ENABLE_SHARED}
     -DENABLE_STATIC=${libjpeg-turbo_ENABLE_STATIC}
-    -DWITH_TOOLS=OFF
     -DWITH_TESTS=OFF)
 if(TLRENDER_NASM)
     list(APPEND libjpeg-turbo_ARGS -DCMAKE_ASM_NASM_COMPILER=${CMAKE_INSTALL_PREFIX}/bin/nasm)
@@ -33,3 +32,17 @@ ExternalProject_Add(
     GIT_TAG ${libjpeg-turbo_GIT_TAG}
     LIST_SEPARATOR |
     CMAKE_ARGS ${libjpeg-turbo_ARGS})
+
+# The command line tools go with the library and there is no option to
+# leave them out, so they are removed after the install. Only the library
+# is wanted; nothing here runs cjpeg.
+set(libjpeg-turbo_TOOLS)
+foreach(tool cjpeg djpeg jpegtran rdjpgcom wrjpgcom tjbench)
+    list(APPEND libjpeg-turbo_TOOLS
+        ${CMAKE_INSTALL_PREFIX}/bin/${tool}${CMAKE_EXECUTABLE_SUFFIX})
+endforeach()
+ExternalProject_Add_Step(
+    libjpeg-turbo libjpeg-turbo-tools
+    COMMAND ${CMAKE_COMMAND} -E rm -f ${libjpeg-turbo_TOOLS}
+    DEPENDEES install
+    ALWAYS OFF)
